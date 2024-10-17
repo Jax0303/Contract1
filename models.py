b82fa2ec4c1f9c290ab79d2792be47a7cbb63e54
georgeyu <des7272@naver.com>
2024-10-17 15:30:41 +0900

    사용자 정보 입력 시 PostgreSQL에 저장, JWT토큰 발급, 토큰을 이용해 사용자 정보 확인 가능

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from passlib.context import CryptContext
 
 Base = declarative_base()
